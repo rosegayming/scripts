532bd4c7f632e0ffbc9eb0e87f8c63488ff1227e
dudeamax99 <dudeamax99@protonmail.com>
2020-12-09 22:10:30 -0500

    Add Powercord detection

--- discord-manager.sh
+++ discord-manager.sh
@@ -9,13 +9,19 @@ URL=$(curl -L --head https://discord.com/api/download/canary\?platform\=linux\&f
 FILENAME=$(basename "$URL")
 CURRENTVERSION=$(cat /usr/share/discord-canary/version.txt)
 NEWVERSION=$(echo "$FILENAME" | grep -Po '(?<=0.0.)[^.]+')
-if ! command -v COMMAND &> /dev/null ;then
+if command -v kdialog &> /dev/null ;then
     DIALOG=kdialog
-elif ! command -v COMMAND &> /dev/null ;then
+elif command -v zenity &> /dev/null ;then
     DIALOG=zenity
 else
     DIALOG=dialog
 fi
+
+if cat /usr/share/discord-canary/resources/app/package.json | grep powercord ;then
+    POWERCORD=1
+fi
+
+OGPWD=$(pwd)
 
 #If discord-canary isntalled, check for updates. if not,  check for updates.
 if [ ! -d "/usr/share/discord-canary/" ] ;then
@@ -34,13 +40,20 @@ if [ ! -d "/usr/share/discord-canary/" ] ;then
 	rm "/tmp/$FILENAME"
 	"$DIALOG" --title "Discord Canary Manager" --msgbox "Discord Canary successfully installed!" 20 60
 elif  [ "$NEWVERSION" -gt "$CURRENTVERSION" ] ;then
-    if "$DIALOG" --title "Discord Canary Manager" --yesno "New Discord Canary Update ($NEWVERSION)!\nInstall it now?" 20 60; then exit ; fi
+    if ! "$DIALOG" --title "Discord Canary Manager" --yesno "New Discord Canary Update ($NEWVERSION)!\nInstall it now?" 20 60; then exit ; fi
     wget -c "$URL" -P /tmp/
 	tar -xzf "/tmp/$FILENAME" -C /usr/share/discord-canary/ --overwrite
 	PASSWD=$("$DIALOG" --title "Discord Canary Manager" --password "Password required to continue" 20 60)
 	echo "$PASSWD" | sudo -S mv /usr/share/discord-canary/discord-canary.desktop /usr/share/applications/
 	echo "$NEWVERSION" > /usr/share/discord-canary/version.txt
 	rm "/tmp/$FILENAME"
+	if [ "$POWERCORD" -eq 1 ] ;then
+        if ! "$DIALOG" --title "Discord Canary Manager" --yesno "Powercord installation detected! Replug Powercord?" 20 60; then exit ; fi
+        PCPATH=$("$DIALOG" --title "Discord Canary Manager" --inputbox "Enter the path to your Powercord installation" "/home/rose/git/powercord")
+        cd "$PCPATH"
+        npm run plug
+        cd "$OGPWD"
+    fi
 	"$DIALOG" --title "Discord Canary Manager" --msgbox "Discord Canary successfully updated\!" 20 60
 fi
 "$DIALOG" --title "Discord Canary Manager" --msgbox "Discord Canary up to date." 20 60
